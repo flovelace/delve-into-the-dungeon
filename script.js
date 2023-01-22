f03af2f3265706ecde624ff6d4df7adac561c97d
flovelace <lovelace.af@gmail.com>
2023-01-21 22:29:29 -0500

    added JS functionality for modal and close modal button

---
+++ script.js
@@ -0,0 +1,25 @@
+// Global variables
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const openModalBtn = document.querySelector('.about-button');
+const closeModalBtn = document.querySelector('.btn-close');
+
+// function that open modals and removes the hidden class
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+// add event listener, pass in click, and the open modal function
+openModalBtn.addEventListener('click', openModal);
+
+// function that closes modal and adds the hidden class
+function closeModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+// event listeners for the close modal button, and overlay
+closeModalBtn.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
